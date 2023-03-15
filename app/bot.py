30a23092b6a9f40e13b6a90c578f415e3a728b24
DonBattery <mikloslorinczi.it@gmail.com>
2023-03-15 13:32:38 +0100

    added app folder and Docker file

---
+++ app/bot.py
@@ -0,0 +1,35 @@
+# This example requires the 'members' privileged intent to use the Member converter
+# and the 'message_content' privileged intent for prefixed commands.
+
+import os
+
+import discord
+from discord.ext import commands
+from vicc import új_vicc
+
+description = """
+Egyszerű szöveges kaland RPG
+"""
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description=description,
+    intents=intents,
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+@bot.command()
+async def vicc(ctx: commands.Context):
+    """Véletlen vicc"""
+    await ctx.send(új_vicc())
+
+bot.run(os.getenv("DISCORD_TOKEN"))
