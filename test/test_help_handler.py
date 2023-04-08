84b8f2f813b209d43dbc6f0c449ce2ab6888151b
DonBattery <mikloslorinczi.it@gmail.com>
2023-04-08 11:00:57 +0200

    updated tests

--- test/test_help_handler.py
+++ test/test_help_handler.py
@@ -14,16 +14,16 @@ from app.handlers import handle_help
 class TestHandleHelp(unittest.TestCase):
 
     def test_single_argument(self):
-        help_text = handle_help("sajt")
+        help_text = handle_help({}, "sajt")
         assert "A `!sajt` nem ismert parancs." in help_text
 
     def test_multiple_arguments(self):
-        help_text = handle_help("help", "vicc")
+        help_text = handle_help({}, "help", "vicc")
         assert "A !help paranccsal információt kaphatsz a játékban használható parancsokról." in help_text
         assert not "A !vicc parancs generál egy új viccet" in help_text
 
     def test_no_arguments(self):
-        help_text = handle_help()
+        help_text = handle_help({})
         assert "Használható parancsok:" in help_text
 
 if __name__ == '__main__':
